f2f44e5bee6d2ad6eee3bbfc0588a95aff91a479
Paul-cmd29 <paulpetrov8510@gmail.com>
2022-08-01 20:21:15 +0300

    inital commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿//Console.WriteLine("Hello, World!");
+
+using System;
+
+namespace Lesson_2
+{
+    class Lesson
+    {
+        static void Main (string[] args)
+        {
+            string Name = "Paul";
+
+            int? Age = 21;
+
+
+            Console.WriteLine($"{Name} is {Age} years old");
+
+
+
+        }
+    }
+}
+
+//string Name = "Paul";
+
+//int? Age = 21;
+
+
+//Console.WriteLine($"{Name} is {Age} years old"); 
+
+
+
+
+
